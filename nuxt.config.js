64053c27b7e6cecd4bc395091dc578491c94c8c6
Loïs Boubault <7613286+Applelo@users.noreply.github.com>
2021-04-17 13:00:31 +0200

    update package

--- nuxt.config.js
+++ nuxt.config.js
@@ -1,49 +1,49 @@
 export default {
-  target: "static",
+  target: 'static',
   head: {
     meta: [
-      { charset: "utf-8" },
+      { charset: 'utf-8' },
       {
-        name: "viewport",
-        content: "width=device-width, initial-scale=1",
+        name: 'viewport',
+        content: 'width=device-width, initial-scale=1',
       },
       {
-        name: "format-detection",
-        content: "telephone=no",
+        name: 'format-detection',
+        content: 'telephone=no',
       },
     ],
     link: [
       {
-        rel: "preconnect",
-        href: "https://fonts.gstatic.com",
+        rel: 'preconnect',
+        href: 'https://fonts.gstatic.com',
       },
       {
-        rel: "stylesheet",
+        rel: 'stylesheet',
         href:
-          "https://fonts.googleapis.com/css2?family=Arvo:ital,wght@0,400;0,700;1,400&display=swap",
+          'https://fonts.googleapis.com/css2?family=Arvo:ital,wght@0,400;0,700;1,400&display=swap',
       },
     ],
   },
   components: true,
-  buildModules: ["@nuxt/typescript-build", "@nuxtjs/style-resources"],
-  css: ["~/assets/scss/base.scss"],
+  buildModules: ['@nuxt/typescript-build', '@nuxtjs/style-resources'],
+  css: ['~/assets/scss/base.scss'],
   styleResources: {
     scss: [
-      "breakpoint-sass/stylesheets/_breakpoint.scss",
-      "./assets/scss/mixins/*.scss",
-      "./assets/scss/vars/*.scss",
+      'breakpoint-sass/stylesheets/_breakpoint.scss',
+      './assets/scss/mixins/*.scss',
+      './assets/scss/vars/*.scss',
     ],
   },
   modules: [
     [
-      "nuxt-i18n",
+      'nuxt-i18n',
       {
         locales: [
-          { code: "en", iso: "en-US", file: "en.js" },
-          { code: "fr", iso: "fr-FR", file: "fr.js" },
+          { code: 'en', iso: 'en-US', file: 'en.js' },
+          { code: 'fr', iso: 'fr-FR', file: 'fr.js' },
         ],
-        defaultLocale: "en",
-        langDir: "~/i18n/",
+        defaultLocale: 'en',
+        langDir: '~/i18n/',
       },
     ],
   ],
