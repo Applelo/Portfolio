9540b4d6f66cf0df5cbefdc940dbedcd08ac5fdc
Loïs Boubault <7613286+Applelo@users.noreply.github.com>
2021-04-26 16:52:54 +0200

    Add project route Fixes #93

---
+++ pages/projects/_slug.vue
@@ -0,0 +1,25 @@
+<template lang="pug">
+  .modal
+</template>
+
+<script lang="ts">
+import Vue from 'vue';
+export default Vue.extend({
+  async asyncData({ $content, params }) {
+    const article = await $content('projects', params.slug).fetch();
+
+    return { article };
+  },
+});
+</script>
+
+<style lang="scss">
+.modal {
+  display: block;
+  position: fixed;
+  top: 0;
+  left: 0;
+  right: 0;
+  bottom: 0;
+}
+</style>
